b0a7e13d3d1f3f97e1fc4cfb36093a66e7b9836e
chandra <chandraramdhanpurnama@gmail.com>
2019-06-20 23:26:51 +0700

    Update

---
+++ vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Illuminate\Auth\Notifications;
+
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPassword extends Notification
+{
+    /**
+     * The password reset token.
+     *
+     * @var string
+     */
+    public $token;
+
+    /**
+     * The callback that should be used to build the mail message.
+     *
+     * @var \Closure|null
+     */
+    public static $toMailCallback;
+
+    /**
+     * Create a notification instance.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        if (static::$toMailCallback) {
+            return call_user_func(static::$toMailCallback, $notifiable, $this->token);
+        }
+
+        return (new MailMessage)
+            ->subject(Lang::getFromJson('Reset Password'))
+            ->line(Lang::getFromJson('Kamu menerima email ini karena kami menerima permintaan bahwa anda ingin me-reset password untuk akun anda.'))
+            ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('password.reset', ['token' => $this->token], false)))
+            ->line(Lang::getFromJson('Tautan reset password ini akan hangus dalam :count menit.', ['count' => config('auth.passwords.users.expire')]))
+            ->line(Lang::getFromJson('Jika kamu tidak meminta untuk mereset password, jangan klik tautan di atas.'));
+    }
+
+    /**
+     * Set a callback that should be used when building the notification mail message.
+     *
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public static function toMailUsing($callback)
+    {
+        static::$toMailCallback = $callback;
+    }
+}
